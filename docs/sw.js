c191b5a2f6915626e5ba0c91ab1bb0ce7d38c118
sczn313-stack <sczn313@gmail.com>
2026-01-21 20:58:32 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,30 @@
+const CACHE = "tap-n-score-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./index.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((hit) => hit || fetch(evt.request))
+  );
+});
